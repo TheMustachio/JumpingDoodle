cc5a1bcca2e4865d0b178e945ebd3a8c9489affd
Eduardo Cea <guayando1207@gmail.com>
2021-10-07 09:29:08 -0400

    a lot of new things - gravity, jumping

--- Classes.py
+++ Classes.py
@@ -4,128 +4,165 @@ from pydraw import *
 class Character:
 
     def __init__(self, s, x_loc, y_loc):
-        self.screen = s
-        self.x = x_loc
-        self.y = y_loc
+        self._screen = s
+        self._x = x_loc
+        self._y = y_loc
 
         # creation of the character
-        self.head = Oval(screen=self.screen, x=self.x - 1, y=self.y, width=50 + 1, height=50)
-        self.head.move(0, -(self.head.height() / 2))
-        self.body = Rectangle(screen=self.screen, x=self.x, y=self.y, width=50, height=65)
+        self._head = Oval(screen=self._screen, x=self._x - 1, y=self._y, width=50 + 1, height=50)
+        self._head.move(0, -(self._head.height() / 2))
+        self._body = Rectangle(screen=self._screen, x=self._x, y=self._y, width=50, height=65)
 
-        self.ring_1 = Rectangle(screen=self.screen, x=self.body.x(), y=self.body.y() + 30,
-                                width=self.body.width(), height=self.body.height() / 9)
-        self.ring_2 = Rectangle(screen=self.screen, x=self.body.x(), y=self.body.y() + 50,
-                                width=self.body.width(), height=self.body.height() / 9)
+        self._ring_1 = Rectangle(screen=self._screen, x=self._body.x(), y=self._body.y() + 30,
+                                 width=self._body.width(), height=self._body.height() / 9)
+        self._ring_2 = Rectangle(screen=self._screen, x=self._body.x(), y=self._body.y() + 50,
+                                 width=self._body.width(), height=self._body.height() / 9)
 
-        self.right_eye = Oval(screen=self.screen, x=self.head.x() + 10, y=self.head.y() + 10, width=5, height=7)
-        self.left_eye = Oval(screen=self.screen, x=self.head.x() + 20, y=self.head.y() + 8, width=5, height=5)
+        self._right_eye = Oval(screen=self._screen, x=self._head.x() + 10, y=self._head.y() + 10, width=5, height=7)
+        self._left_eye = Oval(screen=self._screen, x=self._head.x() + 20, y=self._head.y() + 8, width=5, height=5)
 
-        self.snout = Rectangle(screen=self.screen, x=self.head.x(), y=self.head.y() + (self.head.height() / 2),
-                               width=-30, height=15)
+        self._snout = Rectangle(screen=self._screen, x=self._head.x(), y=self._head.y() + (self._head.height() / 2),
+                                width=-30, height=15)
 
-        self.far_left_leg = Line(self.screen, self.body.x(), self.body.y() + self.body.height(),
-                                 self.body.x(), self.body.y() + self.body.height() + 15)
+        self._far_left_leg = Line(self._screen, self._body.x(), self._body.y() + self._body.height(),
+                                  self._body.x(), self._body.y() + self._body.height() + 15)
 
-        self.left_leg = Line(self.screen, self.body.x() + 15, self.body.y() + self.body.height(),
-                             self.body.x() + 15, self.body.y() + self.body.height() + 15)
+        self._left_leg = Line(self._screen, self._body.x() + 15, self._body.y() + self._body.height(),
+                              self._body.x() + 15, self._body.y() + self._body.height() + 15)
 
-        self.right_leg = Line(self.screen, self.body.x() + self.body.width() - 15, self.body.y() + self.body.height(),
-                              self.body.x() + self.body.width() - 15, self.body.y() + self.body.height() + 15)
+        self._right_leg = Line(self._screen, self._body.x() + self._body.width() - 15,
+                               self._body.y() + self._body.height(),
+                               self._body.x() + self._body.width() - 15, self._body.y() + self._body.height() + 15)
 
-        self.far_right_leg = Line(self.screen, self.body.x() + self.body.width(),
-                                  self.body.y() + self.body.height(),
-                                  self.body.x() + self.body.width(), self.body.y() + self.body.height() + 15)
+        self._far_right_leg = Line(self._screen, self._body.x() + self._body.width(),
+                                   self._body.y() + self._body.height(),
+                                   self._body.x() + self._body.width(), self._body.y() + self._body.height() + 15)
 
-        self.is_left = True
-        self.is_moving = False
+        self._is_left = True
 
         # making the characters color correct.
         body_color = Color(222, 224, 63)
         ring_color = Color(48, 171, 54)
-        self.head.color(body_color)
-        self.body.color(body_color)
-        self.snout.color(body_color)
-        self.ring_1.color(ring_color)
-        self.ring_2.color(ring_color)
+        self._head.color(body_color)
+        self._body.color(body_color)
+        self._snout.color(body_color)
+        self._ring_1.color(ring_color)
+        self._ring_2.color(ring_color)
 
         # adding border around character
         border_color = Color('Black')
-        self.head.border(border_color, width=2)
-        self.body.border(border_color, width=2)
-        self.ring_1.border(border_color, width=2)
-        self.ring_2.border(border_color, width=2)
-        self.snout.border(border_color, width=2)
+        self._head.border(border_color, width=2)
+        self._body.border(border_color, width=2)
+        self._ring_1.border(border_color, width=2)
+        self._ring_2.border(border_color, width=2)
+        self._snout.border(border_color, width=2)
 
-        self.far_left_leg.thickness(2)
-        self.left_leg.thickness(2)
-        self.right_leg.thickness(2)
-        self.far_right_leg.thickness(2)
+        self._far_left_leg.thickness(2)
+        self._left_leg.thickness(2)
+        self._right_leg.thickness(2)
+        self._far_right_leg.thickness(2)
 
-    def get_x(self):
-        if self.is_left:
-            return self.head.x() + self.snout.width()
+        self._width = self._body.width() - self._snout.width()
+        self._height = self._body.height() + (self._head.height() / 2)
+
+    @property
+    def x_value(self):
+        if self._is_left:
+            return self._head.x() + self._snout.width()
         else:
-            return self.head.x()
+            return self._head.x()
 
-    def get_y(self):
-        return self.head.y()
+    @x_value.setter
+    def x_value(self, new_x):
+        self._x = new_x
 
-    def get_width(self):
-        return self.body.width() - self.snout.width()
+    @property
+    def y_value(self):
+        return self._head.y()
 
-    def get_height(self):
-        return self.body.height() + (self.head.height() / 2)
+    @y_value.setter
+    def y_value(self, new_y):
+        self._y = new_y
+
+    @property
+    def width_value(self):
+        return self._width
+
+    @width_value.setter
+    def width_value(self, new_width):
+        self._width = new_width
+
+    @property
+    def height_value(self):
+        return self._height
+
+    @height_value.setter
+    def height_value(self, new_height):
+        self._height = new_height
 
     def move(self, x, y):
-        self.head.move(x, y)
-        self.body.move(x, y)
-        self.ring_1.move(x, y)
-        self.ring_2.move(x, y)
-        self.right_eye.move(x, y)
-        self.left_eye.move(x, y)
-        self.snout.move(x, y)
-        self.far_left_leg.move(x, y)
-        self.left_leg.move(x, y)
-        self.right_leg.move(x, y)
-        self.far_right_leg.move(x, y)
+        self._head.move(x, y)
+        self._body.move(x, y)
+        self._ring_1.move(x, y)
+        self._ring_2.move(x, y)
+        self._right_eye.move(x, y)
+        self._left_eye.move(x, y)
+        self._snout.move(x, y)
+        self._far_left_leg.move(x, y)
+        self._left_leg.move(x, y)
+        self._right_leg.move(x, y)
+        self._far_right_leg.move(x, y)
 
     def moveto(self, x, y):
-        self.head.moveto(x - self.snout.width() - 1, y)
-        self.body.moveto(x - self.snout.width(), y + self.head.height() / 2)
+        self._head.moveto(x - self._snout.width() - 1, y)
+        self._body.moveto(x - self._snout.width(), y + self._head.height() / 2)
 
-        self.ring_1.moveto(self.body.x(), self.body.y() + 30)
-        self.ring_2.moveto(self.body.x(), self.body.y() + 50)
+        self._ring_1.moveto(self._body.x(), self._body.y() + 30)
+        self._ring_2.moveto(self._body.x(), self._body.y() + 50)
 
-        self.right_eye.moveto(self.head.x() + 10, self.head.y() + 10)
-        self.left_eye.moveto(self.head.x() + 20, self.head.y() + 8)
+        self._right_eye.moveto(self._head.x() + 10, self._head.y() + 10)
+        self._left_eye.moveto(self._head.x() + 20, self._head.y() + 8)
 
-        self.snout.moveto(self.head.x(), self.head.y() + (self.head.height() / 2))
+        self._snout.moveto(self._head.x(), self._head.y() + (self._head.height() / 2))
 
-        self.far_left_leg.moveto(self.body.x(), self.body.y() + self.body.height(),
-                                 self.body.x(), self.body.y() + self.body.height() + 15)
-        self.left_leg.moveto(self.body.x() + 15, self.body.y() + self.body.height(),
-                             self.body.x() + 15, self.body.y() + self.body.height() + 15)
-        self.right_leg.moveto(self.body.x() + self.body.width() - 15, self.body.y() + self.body.height(),
-                              self.body.x() + self.body.width() - 15, self.body.y() + self.body.height() + 15)
+        self._far_left_leg.moveto(self._body.x(), self._body.y() + self._body.height(),
+                                  self._body.x(), self._body.y() + self._body.height() + 15)
+        self._left_leg.moveto(self._body.x() + 15, self._body.y() + self._body.height(),
+                              self._body.x() + 15, self._body.y() + self._body.height() + 15)
+        self._right_leg.moveto(self._body.x() + self._body.width() - 15, self._body.y() + self._body.height(),
+                               self._body.x() + self._body.width() - 15, self._body.y() + self._body.height() + 15)
 
-        self.far_right_leg.moveto(self.body.x() + self.body.width(), self.body.y() + self.body.height(),
-                                  self.body.x() + self.body.width(), self.body.y() + self.body.height() + 15)
+        self._far_right_leg.moveto(self._body.x() + self._body.width(), self._body.y() + self._body.height(),
+                                   self._body.x() + self._body.width(), self._body.y() + self._body.height() + 15)
 
     def turn_right(self):
-        self.snout.moveto(self.body.x() + self.body.width() - self.snout.width(), self.snout.y())
-        self.left_eye.moveto(self.head.x() + self.head.width() - 25, self.left_eye.y())
-        self.right_eye.moveto(self.body.x() + self.body.width() - 15, self.right_eye.y())
-        self.is_left = False
+        self._snout.moveto(self._body.x() + self._body.width() - self._snout.width(), self._snout.y())
+        self._left_eye.moveto(self._head.x() + self._head.width() - 25, self._left_eye.y())
+        self._right_eye.moveto(self._body.x() + self._body.width() - 15, self._right_eye.y())
+        self._is_left = False
 
     def turn_left(self):
-        self.snout.moveto(self.body.x(), self.snout.y())
-        self.left_eye.moveto(self.head.x() + 20, self.left_eye.y())
-        self.right_eye.moveto(self.head.x() + 10, self.right_eye.y())
-        self.is_left = True
+        self._snout.moveto(self._body.x(), self._snout.y())
+        self._left_eye.moveto(self._head.x() + 20, self._left_eye.y())
+        self._right_eye.moveto(self._head.x() + 10, self._right_eye.y())
+        self._is_left = True
 
-    def moving(self):
-        return self.is_moving
+    def jump(self):
+        pass
 
-    def change_moving(self, boolean):
-        self.is_moving = boolean
+
+class Platform:
+
+    def __init__(self, screen, x, y, kind):
+        self._screen = screen
+        self._x = x
+        self._y = y
+        self._kind = kind
+
+        if kind == 'non-movable':
+            self.platform = Rectangle(self._screen, self._x, self._y, 100, 20, Color('green'))
+
+    def overlap(self, doodle):
+        if self.platform.overlaps(doodle):
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pydraw import *
-import datetime
-from Classes import Character
+# import datetime
+from Classes import Character, Platform
 
 # setup
 screen = Screen(600, 800, 'Doodle Jump')
@@ -10,39 +10,68 @@ Medici_Ivory = Color(243, 233, 215)
 screen.color(Medici_Ivory)
 screen.grid(rows=30, cols=30, helpers=False)
 
+doodle = Character(screen, screen.width() / 2, screen.height() / 2)
+doodle.alive = True
+doodle.horizontal_vector = 0
+doodle.vertical_vector = -10
 
-def character(x, y):
-    return Character(screen, x, y)
+
+def fall():
+    doodle.move(0, doodle.vertical_vector)
+    if doodle.vertical_vector <= 10:
+        doodle.vertical_vector += 1
+
+
+plat = Platform(screen, screen.width()/2, 3*screen.height()/4, 'non-movable')
+
+
+def check_game():
+    if doodle.alive:
+        if doodle.y_value > screen.height():
+            doodle.alive = False
 
 
 # key inputs
 def keydown(key):
     global running
-    x_vector = 15
     if key == 'left':
-        doodle.move(-x_vector, 0)
+        doodle.horizontal_vector = -15
         doodle.turn_left()
 
-        if doodle.get_x() <= 0:
-            doodle.moveto(screen.width() - doodle.get_width() - 10, doodle.get_y())
-
     if key == 'right':
-        doodle.move(x_vector, 0)
+        doodle.horizontal_vector = 15
         doodle.turn_right()
 
-        if doodle.get_x() + doodle.get_width() >= screen.width():
-            doodle.moveto(-25, doodle.get_y())
-
-        doodle.turn_right()
+    if key == 'space':
+        doodle.vertical_vector = -10
     if key == 'q':
         running = False
 
 
-# creating the character
-doodle = character(screen.width() / 2, screen.height() / 2)
+def keyup(key):
+    if key == 'left':
+        doodle.horizontal_vector = 0
+    if key == 'right':
+        doodle.horizontal_vector = 0
+
+
 # run method
 running = True
 while running:
+    # checking to see if the game will continue going
+    check_game()
+
+    if doodle.alive:
+        fall()
+        doodle.move(doodle.horizontal_vector, 0)
+
+        if doodle.x_value <= 0:
+            doodle.moveto(screen.width() - doodle.width_value - 10, doodle.y_value)
+
+        if doodle.x_value + doodle.width_value >= screen.width():
+            doodle.moveto(-25, doodle.y_value)
+            doodle.turn_right()
+
     # this updates the screen for any movement
     screen.update()
     # this listens for any inputs from the user
@@ -51,9 +80,5 @@ while running:
     screen.sleep(1 / 60)
 
 print('ending program')
-
-file = open('scoreboard.txt')
-for i in range(2):
-    print(file.readline())
 
 screen.exit()
